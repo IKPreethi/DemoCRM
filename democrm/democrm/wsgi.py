95071d942bdf7f60b8716f24b2422ad61f4136b9
IKPreethi <preethiik06@gmail.com>
2024-04-01 17:34:35 -0500

    initial commit

---
+++ democrm/democrm/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for democrm project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'democrm.settings')
+
+application = get_wsgi_application()
