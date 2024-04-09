64429d443caad83e40a88244762ac0b0e1a0c042
IKPreethi <preethiik06@gmail.com>
2024-04-09 01:22:26 -0500

    Leads

--- democrm/democrm/urls.py
+++ democrm/democrm/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
 from django.contrib.auth import views
+from django.urls import path, include
+
 from core.views import index, about
 from userprof.views import signup
 
 urlpatterns = [
-    path('', index, name ='index'),
-    path('about/', about, name ='about'),
+    path('', index, name='index'),
+    path('dashboard/leads/', include('lead.urls')),
     path('dashboard/', include('dashboard.urls')),
-    path('sign-up/', signup, name ='signup'),
+    path('about/', about, name='about'),
+    path('sign-up/', signup, name='signup'),
     path('log-in/', views.LoginView.as_view(template_name='userprof/login.html'), name='login'),
-    path('log-out/', views.LoginView.as_view(), name='logout'),
+    path('log-out/', views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
